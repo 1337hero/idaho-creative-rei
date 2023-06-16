a3adb1c4671742ab339d3944cfda410fcf6f1203
Mike Key <mike@1337hero.com>
2023-06-16 12:54:40 -0600

    first commit

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "siteTitle": "Idaho Creative Real Estate Investors",
+    "author": "Mike Key",
+    "description": "",
+
+    /*-- Intro --*/
+    "pageTitle": "Idaho Creative Real Estate Investors",
+    "subTitle": "",
+
+}
